41fa38d492ef406e9ce69eaac47edd085af8b9f4
Nawaf AlGhamdi <98660296+Nawaf9529@users.noreply.github.com>
2023-06-09 23:21:15 +0300

    Add files via upload

---
+++ src/main/java/sa/edu/kau/fcit/cpit252/logging/Logger.java
@@ -0,0 +1,40 @@
+package sa.edu.kau.fcit.cpit252.logging;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+
+public class Logger {
+    private static Logger LoggerInstance;
+    DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm-ss");
+    String currentDateTimeString = LocalDateTime.now().format(myFormatter);
+    private final String logFile = currentDateTimeString + ".txt";
+    private PrintWriter writer;
+
+    private Logger() {
+        try {
+            FileWriter fw = new FileWriter(logFile);
+            writer = new PrintWriter(fw, true);
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static Logger getLoggerInstance() {
+        if (LoggerInstance==null) {
+            LoggerInstance=new Logger();
+        }
+        return LoggerInstance;
+    }
+    
+   
+    public void info (String message) {
+        writer.println("INFO: " + message);
+    }
+    public void error (String message) {
+        writer.println("Error: " + message);
+    }
+}
